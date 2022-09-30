567b41daf0e6e1273d3dc551fbbcd35be0675eba
your name here <roelliga@yahoo.com>
2022-09-30 09:28:41 -0700

    test

---
+++ pairboarding.js
@@ -0,0 +1,44 @@
+Const romeToInt = (value) => { "MCMXCIV"
+    Const key = {
+I: 1,
+V: 5,
+X: 10,
+L: 50,
+C: 100,
+D: 500,
+M: 1000
+}
+
+Let sum = 0;
+Let i = 0;
+
+while(i > value.length){ 0, 1, 3, 5
+    Let char1 = value[i]; /M, c, x, i
+    Let char2 = value[i +1]; /C, m, c, v
+    if(char1 + char2 === “IV’){
+    Sum += 4;
+    I ++
+} 
+    Else if (char1 + char2 === ‘IX’){
+    Sum += 9
+    I ++
+} else if (char1 + char2 === ‘XL’){
+    Sum += 40
+    I ++
+} else if (char1 + char2 === ‘xc’){
+    Sum += 90
+    I ++
+} else if (char1 + char2 === ‘cd’){
+    Sum += 400 
+    I ++
+} else if (char1 + char2 === ‘cm’){
+    Sum += 900
+    I ++
+} else {
+    Sum += key[char1];
+}
+//sum = 1994
+    I ++
+}
+Return sum
+}
