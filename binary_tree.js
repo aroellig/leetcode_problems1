b198b06de4e5bd6c173e9a2f521fa4d129dcbf82
your name here <roelliga@yahoo.com>
2022-10-03 11:16:03 -0700

    breadthfirstvalues

--- binary_tree.js
+++ binary_tree.js
@@ -20,3 +20,17 @@ const depthFirstValues = (root) => {
   };
   //
   
+  //
+  const breadthFirstValues = (root) => {
+    if(root === null) return [];
+    let result = [];
+    let queue = [root];
+    while(queue.length > 0){
+      let current = queue.shift();
+      result.push (current.val)
+      if(current.left) queue.push(current.left);
+      if(current.right) queue.push(current.right);
+    }
+    return result
+  }
+  
