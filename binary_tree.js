fa1328253b4ee45bf2486f13baa86ef9f19f0ef3
your name here <roelliga@yahoo.com>
2022-10-03 11:13:01 -0700

    depthfirstvalues

--- binary_tree.js
+++ binary_tree.js
@@ -11,3 +11,11 @@ const depthFirstValues = (root) => {
     }
     return result;
   };
+
+  onst depthFirstValues = (root) => {
+    if (root === null) return [];
+    const leftValues = depthFirstValues(root.left);
+    const rightValues = depthFirstValues(root.right);
+    return [root.val, ...leftValues, ...rightValues];
+  };
+  
