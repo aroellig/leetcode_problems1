8e7d789a5fb3a1afa4f22f4f67b5d9e7b066d837
your name here <roelliga@yahoo.com>
2022-10-06 11:35:14 -0700

    maxPathSum

--- binary_tree.js
+++ binary_tree.js
@@ -83,3 +83,13 @@ const depthFirstValues = (root) => {
           return min
         };
         //
+
+        //
+        const maxPathSum = (root) => {
+          if(root === null) return -Infinity
+          if(root.left === null && root.right === null) return root.val;
+         const maxChild = Math.max(maxPathSum(root.left),maxPathSum(root.right));
+          return root.val + maxChild
+        
+        };
+        //
