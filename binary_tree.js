240d26bb2b9ec163214e52f8fbe33044fdefe7bf
aroellig1 <aroellig@upchannels.com>
2022-10-10 11:34:22 -0700

    howHigh

--- binary_tree.js
+++ binary_tree.js
@@ -147,3 +147,14 @@ const depthFirstValues = (root) => {
         };
 
         //
+
+        //
+        time: O(n)
+        space: O(n)
+        const howHigh = (node) => {
+          if(node === null) return -1
+            let leftHeight = howHigh(node.left);
+            let rightHeight = howHigh(node.right);
+            return (1 + Math.max(leftHeight, rightHeight))
+          };
+          //
