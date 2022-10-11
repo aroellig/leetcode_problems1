6ef55f4ae7e9d665c43ba372da9ca21069618cd9
aroellig1 <aroellig@upchannels.com>
2022-10-11 11:39:11 -0700

    alltreepaths

--- binary_tree.js
+++ binary_tree.js
@@ -177,5 +177,19 @@ const depthFirstValues = (root) => {
             //
             time: O(n)
             space: O(n)
-
+            const allTreePaths = (root) => {
+              if(root === null) return []; 
+             if(root.left === null && root.right === null) return [[root.val]];
+               const paths = [];
+              const leftSubPaths = allTreePaths(root.left);
+               for(let subPath of leftSubPaths){
+                 paths.push([root.val, ...subPath]);
+               }
+              const rightSubPaths = allTreePaths(root.right);
+               for(let subPath of rightSubPaths){
+                 paths.push([root.val, ...subPath])
+               }
+               
+               return paths;
+             };
             //
