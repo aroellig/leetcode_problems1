0939335d8983b3900b98236d64926c291f5e3d38
aroellig1 <roelliga@yahoo.com>
2022-10-11 12:13:37 -0700

    treeLevels

--- binary_tree.js
+++ binary_tree.js
@@ -213,5 +213,21 @@ const depthFirstValues = (root) => {
               }
               return levels;
             };
+            const treeLevels = (root) => {
+              if(root === null) return [];
+              const levels = []
+              const queue = [{node:root, levelNum: 0}];
+              while(queue.length > 0){
+               const {node, levelNum} = queue.shift();
+                if(levels.length === levelNum){
+                  levels.push([node.val])
+                } else{
+                  levels[levelNum].push(node.val);
+                }
+                if(node.left !== null) queue.push({node: node.left, levelNum: levelNum + 1})
+                if(node.right !== null) queue.push({node: node.right, levelNum: levelNum + 1})
+              }
+              return levels;
+            };
             
             //
