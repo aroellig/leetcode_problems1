e05701ea509e7ce492a2c6894afb468fe86797ef
aroellig1 <roelliga@yahoo.com>
2022-10-12 11:08:33 -0700

    leaf list

--- binary_tree.js
+++ binary_tree.js
@@ -285,4 +285,18 @@ const depthFirstValues = (root) => {
             //
             time: O(n)
             space: O(n)
+            const leafList = (root) => {
+              if(root === null) return [];
+              const leaves = [];
+              const stack = [root];
+              while(stack.length > 0){
+               const current = stack.pop();
+                if (current.left === null && current.right === null){
+                  leaves.push(current.val)
+                }
+                if(current.right !== null) stack.push(current.right);
+                if(current.left !== null) stack.push(current.left);
+              }
+              return leaves
+            };
             //
