47106f715599a6ebaf016999dcfa76e72ac0d565
aroellig1 <roelliga@yahoo.com>
2022-10-17 10:13:31 -0700

    sumList

--- linkedlist.js
+++ linkedlist.js
@@ -22,3 +22,15 @@ const linkedListValues = (head) => {
         }
         
     //
+
+    //
+    const sumList = (head) => {
+        let sum = 0;
+          let current = head;
+          while(current !== null){
+            sum += current.val;
+            current = current.next
+          }
+          return sum
+        };
+//        
