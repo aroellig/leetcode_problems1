ff5d5836df5563463bccf3532f69f47b6eb09bcd
aroellig1 <roelliga@yahoo.com>
2022-10-17 10:12:17 -0700

    linkedlistvalues

--- linkedlist.js
+++ linkedlist.js
@@ -8,4 +8,17 @@ const linkedListValues = (head) => {
       }
       return arr
     };
+
+    const linkedListValues = (head) => {
+        let arr = [];
+          fillValues(head, arr);
+          return arr
+        };
+        
+        const fillValues = (head, arr) => {
+          if(head === null) return;
+          arr.push(head.val);
+          fillValues(head.next, arr);
+        }
+        
     //
