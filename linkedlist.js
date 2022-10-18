4006d1368fa979d07072cef6434301d7d467d044
aroellig1 <roelliga@yahoo.com>
2022-10-18 11:31:05 -0700

    zipperlist

--- linkedlist.js
+++ linkedlist.js
@@ -124,5 +124,18 @@ const zipperLists = (head1, head2) => {
       if(current1 !== null) tail.next = current1;
       if(current2 !== null) tail.next = current2;
       return head1;
+
+      const zipperLists = (head1, head2) => {
+        if(head1 === null && head2 === null) return null;
+          if(head1 === null) return head2;
+          if(head2 === null) return head1;
+          const next1 = head1.next;
+          const next2 = head2.next;
+          head1.next = head2;
+          
+          head2.next = zipperLists(next1, next2);
+          
+          return head1;
+        };
     };
     //
